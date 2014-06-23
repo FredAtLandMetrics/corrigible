65a34d555e0748f18eefbf71e53051381de57a14
FredAtLandmetrics <fred@landmetrics.com>
2014-06-23 00:00:48 -0400

    added test for basic accurate hosts file generation

--- test/test_machine_config.py
+++ test/test_machine_config.py
@@ -3,6 +3,7 @@
 import unittest
 import os
 import copy
+import re
 from subprocess import call
 
 script_dirpath = os.path.dirname( os.path.dirname( __file__ ) )
@@ -29,13 +30,57 @@ class TestMachineConfig(unittest.TestCase):
             
         call([corrigible_exec_filepath, "test_machine_config", "--generate-files-only", "--playbook-output-file={}".format(PLAYBOOK_FILEPATH__MACHINECONF_TEST), "--hosts-output-file={}".format(HOSTS_FILEPATH__MACHINECONF_TEST)], env=os.environ.copy())
         
+    def hosts_groups_from_file(self, hosts_filepath):
+        ret = []
+        with open(hosts_filepath) as f:
+            lines = f.readlines()
+            for l in lines:
+                m = re.match(r"^\[(.*)\]", l)
+                if m is not None:
+                    ret.append(m.group(1))
+        return ret
     
-    def test_machine_config(self):
+    def hostgroup_lines(self, hosts_filepath, hostgroup):
+        ret = []
+        with open(hosts_filepath) as f:
+            lines = f.readlines()
+            found = False
+            for l in lines:
+                
+                # skip lines until found
+                if not found:
+                    m = re.match(r"^\[{}\]".format(hostgroup), l)
+                    if m is not None:
+                        found = True
+                    continue
+                
+                # capture lines until next group
+                m = re.match(r"^\[.*\]", l)
+                if m is None and bool(l.strip()):
+                    ret.append(l.strip())
+                    continue
+                
+                # if we get this far, it's over, return
+                break
+            
+        if not bool(ret):
+            ret = None
+            
+        return ret
+    
+    def test_machine_config_output_files_exist(self):
         self.regen_test_machine_config_files()
-        #print "PLAYBOOK_FILEPATH__MACHINECONF_TEST, HOSTS_FILEPATH__MACHINECONF_TEST: {}, {}".format(PLAYBOOK_FILEPATH__MACHINECONF_TEST, HOSTS_FILEPATH__MACHINECONF_TEST)
         self.assertTrue(os.path.isfile(PLAYBOOK_FILEPATH__MACHINECONF_TEST))
         self.assertTrue(os.path.isfile(HOSTS_FILEPATH__MACHINECONF_TEST))
         
+    def test_machine_config_hosts_file_accurate(self):
+        self.regen_test_machine_config_files()
+        hostgroups = self.hosts_groups_from_file(HOSTS_FILEPATH__MACHINECONF_TEST)
+        self.assertTrue('all' in hostgroups)
+        self.assertTrue(len(hostgroups) == 1)
+        lines = self.hostgroup_lines(HOSTS_FILEPATH__MACHINECONF_TEST,'all')
+        self.assertTrue(len(lines) == 1)
+        self.assertTrue(lines[0] == "testhost ansible_ssh_host=1.2.3.4")
 
 if __name__ == '__main__':
     unittest.main()   
