8d3fea1557d55b70747948a29aa766c46d2e7f8c
FredAtLandmetrics <fred@landmetrics.com>
2014-07-07 00:25:58 -0400

    made default paths match what is in readme

--- lib/dirpaths.py
+++ lib/dirpaths.py
@@ -10,10 +10,13 @@ def envvar_cap_var(environment_variable_name, **kwargs):
             return None
     
 def machines_dirpath():
-    return envvar_cap_var('CORRIGIBLE_MACHINES',default='/usr/local/etc/corrigible/machines')
+    return envvar_cap_var('CORRIGIBLE_MACHINES',default='{}/machines'.format(corrigible_path()))
 
 def directives_dirpath():
-    return envvar_cap_var('CORRIGIBLE_DIRECTIVES',default='/usr/local/etc/corrigible/directives')
+    return envvar_cap_var('CORRIGIBLE_DIRECTIVES',default='{}/directives'.format(corrigible_path()))
 
 def files_dirpath():
-    return envvar_cap_var('CORRIGIBLE_FILES',default='/usr/local/etc/corrigible/files')
+    return envvar_cap_var('CORRIGIBLE_FILES',default='{}/files'.format(corrigible_path()))
+
+def corrigible_path():
+    return envvar_cap_var('CORRIGIBLE_PATH',default='/usr/local/etc/corrigible')
