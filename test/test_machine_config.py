7c0a4bc0dbfe63cd3799d5f922efe13047a2cfaa
FredAtLandmetrics <fred@landmetrics.com>
2014-07-06 22:15:15 -0400

    added method doc comments to test methods

--- test/test_machine_config.py
+++ test/test_machine_config.py
@@ -25,6 +25,7 @@ HOSTS_FILEPATH__MACHINECONF_TEST = "/tmp/corrigible-test-hosts-output.hosts"
 class TestMachineConfig(unittest.TestCase):
 
     def rerun_corrigible(self, **kwargs):
+        """re-run corrigible for a given machine config file"""
         # remove the test output file if it exists
         if os.path.isfile(PLAYBOOK_FILEPATH__MACHINECONF_TEST):
             os.remove(PLAYBOOK_FILEPATH__MACHINECONF_TEST)
@@ -39,14 +40,17 @@ class TestMachineConfig(unittest.TestCase):
         
 
     def regen_test_hostsfile_gen_files(self, **kwargs):
+        """re-run corrigible for the hostsfile generation test config"""
         self.rerun_corrigible(machine_config="test_hostsfile_generation",
                               generate_files_only=True)
         
     def regen_test_simple_directives(self, **kwargs):
+        """re-run corrigible for the simple directive test config"""
         self.rerun_corrigible(machine_config="test_simple_directives",
                               generate_files_only=True)
         
     def hosts_groups_from_file(self, hosts_filepath):
+        """given a path to an ansible hosts file, return the list of all host groups in the hostsfile"""
         ret = []
         with open(hosts_filepath) as f:
             lines = f.readlines()
@@ -57,6 +61,7 @@ class TestMachineConfig(unittest.TestCase):
         return ret
     
     def hostgroup_lines(self, hosts_filepath, hostgroup):
+        """given a path to an ansible hosts file and the name of a hostgroup in that file, return a list of all the host lines in the hostgroup section"""
         ret = []
         with open(hosts_filepath) as f:
             lines = f.readlines()
@@ -85,17 +90,20 @@ class TestMachineConfig(unittest.TestCase):
         return ret
     
     def playbook_as_struct(self):
+        """read the playbook referred to by PLAYBOOK_FILEPATH__MACHINECONF_TEST as a yaml file and return the struct"""
         ret = None
         with open(PLAYBOOK_FILEPATH__MACHINECONF_TEST, 'r') as fh:
             ret = yaml.load(fh)
         return ret
         
     def test_machine_config_output_files_exist(self):
+        """test that the output files exist after rerunning corrigible using the hostsfile generation test machine config"""
         self.regen_test_hostsfile_gen_files()
         self.assertTrue(os.path.isfile(PLAYBOOK_FILEPATH__MACHINECONF_TEST))
         self.assertTrue(os.path.isfile(HOSTS_FILEPATH__MACHINECONF_TEST))
         
     def test_machine_config_hosts_file_accurate(self):
+        """test that the generated hosts file is accurate after rerunning corrigible using the hostsfile generation test machine config"""
         self.regen_test_hostsfile_gen_files()
         hostgroups = self.hosts_groups_from_file(HOSTS_FILEPATH__MACHINECONF_TEST)
         self.assertTrue('all' in hostgroups)
@@ -105,19 +113,20 @@ class TestMachineConfig(unittest.TestCase):
         self.assertTrue(lines[0] == "testhost ansible_ssh_host=1.2.3.4")
         
     def test_directive_index(self):
+        """test that the directive index method in the provision files lib is properly returning the index specified in the directive filename"""
         dt_index = directive_index('directives_test')
-        #print "dt_index: {}".format(dt_index)
         self.assertTrue(dt_index == 57)
         d_index = directive_index('apt_upgrade')
-        #print "d_index: {}".format(d_index)
         self.assertTrue(d_index == 19)
         
     def test_directive_filepath(self):
+        """test that the directive filepath method in the provision files lib is properly returning the filepath to the directive file"""
         dt_filepath = directive_filepath('directives_test');
         print "dt_filepath: {}".format(dt_filepath)
         self.assertTrue(dt_filepath == os.path.abspath(os.path.join(script_dirpath,'resources','directives','57_directives_test.directive.yml')))
         
     def test_parameter_substitution(self):
+        """after re-running corrigible on the simple directives test machine config, test that basic parameter substitution is working"""
         self.regen_test_simple_directives()
         self.assertTrue(os.path.isfile(PLAYBOOK_FILEPATH__MACHINECONF_TEST))
         self.assertTrue(os.path.isfile(HOSTS_FILEPATH__MACHINECONF_TEST))
