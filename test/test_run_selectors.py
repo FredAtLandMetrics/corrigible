dc2d7e4860d4a27485f81b385c237851ef5fffac
FredAtLandmetrics <fred@landmetrics.com>
2014-07-23 01:21:22 -0400

    minor bugfixes to get run selector test to run successfully

--- test/test_run_selectors.py
+++ test/test_run_selectors.py
@@ -3,26 +3,26 @@
 import unittest
 import os
 
-from corrigible.lib.provision_files import directive_index, directive_filepath
+from corrigible.lib.plan import plan_index, plan_filepath
 from corrigible.test.lib.corrigible_test import CorrigibleTest
 
 script_dirpath = os.path.dirname( os.path.dirname( __file__ ) )
-machine_config_dirpath = os.path.join(script_dirpath,'resources','machines')
-directives_config_dirpath = os.path.join(script_dirpath,'resources','directives')
+system_config_dirpath = os.path.join(script_dirpath,'resources','systems')
+plans_config_dirpath = os.path.join(script_dirpath,'resources','plans')
 files_config_dirpath = os.path.join(script_dirpath,'resources','files')
 corrigible_exec_filepath = os.path.join(script_dirpath, '..', 'corrigible')
 
-os.environ['CORRIGIBLE_MACHINES'] = machine_config_dirpath
-os.environ['CORRIGIBLE_DIRECTIVES'] = directives_config_dirpath
+os.environ['CORRIGIBLE_SYSTEMS'] = system_config_dirpath
+os.environ['CORRIGIBLE_PLANS'] = plans_config_dirpath
 os.environ['CORRIGIBLE_FILES'] = files_config_dirpath
 
-PLAYBOOK_FILEPATH__MACHINECONF_TEST = "/tmp/corrigible-test-output.yml"
-HOSTS_FILEPATH__MACHINECONF_TEST = "/tmp/corrigible-test-hosts-output.hosts"
+PLAYBOOK_FILEPATH__SYSTEMCONF_TEST = "/tmp/corrigible-test-output.yml"
+HOSTS_FILEPATH__SYSTEMCONF_TEST = "/tmp/corrigible-test-hosts-output.hosts"
 
 class TestRunSelectors(CorrigibleTest):
     def setUp(self):
-        self.output_playbook_filepath = PLAYBOOK_FILEPATH__MACHINECONF_TEST
-        self.output_hostsfile_filepath = HOSTS_FILEPATH__MACHINECONF_TEST
+        self.output_playbook_filepath = PLAYBOOK_FILEPATH__SYSTEMCONF_TEST
+        self.output_hostsfile_filepath = HOSTS_FILEPATH__SYSTEMCONF_TEST
         self.corrigible_exec_filepath = corrigible_exec_filepath
         
     def _assert_hostsmap(self, incmap):
@@ -37,7 +37,7 @@ class TestRunSelectors(CorrigibleTest):
                 self.assertTrue(hostname in hosts)
         
     def test_hosts(self, **kwargs):
-        self.rerun_corrigible(machine_config="test_rs_hosts",
+        self.rerun_corrigible(system_config="test_rs_hosts",
                               generate_files_only=True)
         incmap =  [ [ 'a', True ],
                     [ 'b', True ],
@@ -48,7 +48,7 @@ class TestRunSelectors(CorrigibleTest):
         
         # determine that the hosts file looks like it should with no run selectors
         
-        self.rerun_corrigible(machine_config="test_rs_hosts",
+        self.rerun_corrigible(system_config="test_rs_hosts",
                               generate_files_only=True,
                               run_selectors="testrs5")
         incmap =  [ [ 'a', True ],
@@ -58,15 +58,15 @@ class TestRunSelectors(CorrigibleTest):
                     [ 'e', True ] ]
         self._assert_hostsmap(incmap)
         
-        #self.rerun_corrigible(machine_config="test_rs_hosts",
+        #self.rerun_corrigible(system_config="test_rs_hosts",
                               #generate_files_only=True,
                               #run_selectors="testrs5")
-        #self.rerun_corrigible(machine_config="test_rs_hosts",
+        #self.rerun_corrigible(system_config="test_rs_hosts",
                               #generate_files_only=True,
                               #run_selectors="testrs1,testrs5")
                               
-    def test_directives(self):
-        self.rerun_corrigible(machine_config="test_rs_hosts",
+    def test_plans(self):
+        self.rerun_corrigible(system_config="test_rs_hosts",
                               generate_files_only=True)
         s = self.playbook_as_struct()
         self.assertTrue(type(s) is list and len(s) == 1)
@@ -76,7 +76,7 @@ class TestRunSelectors(CorrigibleTest):
         self.assertTrue(type(s[0]['tasks'][0]) is dict)
         self.assertTrue('apt' in s[0]['tasks'][0].keys())
         self.assertFalse('cron' in s[0]['tasks'][0].keys())
-        self.rerun_corrigible(machine_config="test_rs_hosts",
+        self.rerun_corrigible(system_config="test_rs_hosts",
                               generate_files_only=True,
                               run_selectors="update_dnsservers")
         s = self.playbook_as_struct()
