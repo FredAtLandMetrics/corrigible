2d37e766b97b570673d436b9e2903c1778dbb3c2
FredAtLandmetrics <fred@landmetrics.com>
2014-08-12 19:45:43 -0400

    updated test to account for additional hash-related instructions in playbook

--- test/test_inline_ansible_plan.py
+++ test/test_inline_ansible_plan.py
@@ -35,11 +35,11 @@ class TestInlineAnsiblePlanSystemConfig(CorrigibleTest):
         self.regen()
         s = self.playbook_as_struct()
         tasksrec = {}
-        self.assertTrue(type(s) is list and len(s) > 0)
-        self.assertTrue(type(s[0]) is dict and 'tasks' in s[0])
-        self.assertTrue(type(s[0]['tasks']) is list and len(s[0]['tasks']) > 0)
-        self.assertTrue(type(s[0]['tasks'][0]) is dict)
-        self.assertTrue('apt' in s[0]['tasks'][0])
+        self.assertTrue(type(s) is list and len(s) > 1)
+        self.assertTrue(type(s[1]) is dict and 'tasks' in s[0])
+        self.assertTrue(type(s[1]['tasks']) is list and len(s[0]['tasks']) > 0)
+        self.assertTrue(type(s[1]['tasks'][0]) is dict)
+        self.assertTrue('apt' in s[1]['tasks'][0])
         
         
 if __name__ == '__main__':
