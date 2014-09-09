55770fb1b6ef8a0d336a9618dcd5dc5374f1ddec
Fred McDavid <fred@landmetrics.com>
2014-09-09 00:19:27 -0400

    minor bugfix relating to rocket mode hashes

--- test/test_simple_system_config.py
+++ test/test_simple_system_config.py
@@ -30,18 +30,18 @@ class TestSimpleSystemConfig(CorrigibleTest):
         """re-run corrigible for the hostsfile generation test config"""
         self.rerun_corrigible(system_config="test_hostsfile_generation",
                               generate_files_only=True)
-        
+
     def regen_test_simple_plans(self, **kwargs):
         """re-run corrigible for the simple plan test config"""
         self.rerun_corrigible(system_config="test_simple_plans",
                               generate_files_only=True)
-        
+
     def test_system_config_output_files_exist(self):
         """test that the output files exist after rerunning corrigible using the hostsfile generation test system config"""
         self.regen_test_simple_plans()
         self.assertTrue(os.path.isfile(self.output_playbook_filepath))
         self.assertTrue(os.path.isfile(self.output_hostsfile_filepath))
-        
+
     def test_system_config_hosts_file_accurate(self):
         """test that the generated hosts file is accurate after rerunning corrigible using the hostsfile generation test system config"""
         self.regen_test_hostsfile_gen_files()
@@ -51,28 +51,29 @@ class TestSimpleSystemConfig(CorrigibleTest):
         lines = self.hostgroup_lines(self.output_hostsfile_filepath,'all')
         self.assertTrue(len(lines) == 1)
         self.assertTrue(lines[0] == "testhost ansible_ssh_host=1.2.3.4")
-        
+
     def test_plan_index(self):
         """test that the plan index method in the provision files lib is properly returning the index specified in the plan filename"""
         dt_index = plan_index('plans_test')
         self.assertTrue(dt_index == 57)
         d_index = plan_index('apt_upgrade')
         self.assertTrue(d_index == 19)
-        
+
     def test_plan_filepath(self):
         """test that the plan filepath method in the provision files lib is properly returning the filepath to the plan file"""
         dt_filepath = plan_filepath('plans_test');
         #print "dt_filepath: {}".format(dt_filepath)
         self.assertTrue(dt_filepath == os.path.abspath(os.path.join(script_dirpath,'resources','plans','57_plans_test.plan.yml')))
-        
+
     def test_parameter_substitution(self):
         """after re-running corrigible on the simple plans test system config, test that basic parameter substitution is working"""
         self.regen_test_simple_plans()
         self.assertTrue(os.path.isfile(self.output_playbook_filepath))
         self.assertTrue(os.path.isfile(self.output_hostsfile_filepath))
         s = self.playbook_as_struct()
-        self.assertTrue(s[0]['user'] == 'ubuntu')
-        self.assertTrue(s[0]['sudo'] == True)
+        print "s: ".format(s)
+        self.assertTrue(s[1]['user'] == 'ubuntu')
+        self.assertTrue(s[1]['sudo'] == True)
 
     def test_plan_ordering_by_index(self):
         """after re-running corrigible on the simple plans test system config, test that the plans are ordered as per the index indicated is each's filename"""
@@ -80,6 +81,7 @@ class TestSimpleSystemConfig(CorrigibleTest):
         self.assertTrue(os.path.isfile(self.output_playbook_filepath))
         self.assertTrue(os.path.isfile(self.output_hostsfile_filepath))
         s = self.playbook_as_struct()
+        print "s: ".format(s)
         self.assertTrue('user' in s[2]['tasks'][0])
         self.assertFalse('user' in s[4]['tasks'][0])
         self.assertFalse('user' in s[6]['tasks'][0])
@@ -89,7 +91,7 @@ class TestSimpleSystemConfig(CorrigibleTest):
         self.assertTrue('apt' in s[6]['tasks'][0])
         self.assertFalse('apt' in s[4]['tasks'][0])
         self.assertFalse('apt' in s[2]['tasks'][0])
-        
+
         # files
         self.assertTrue('copy' in s[1]['tasks'][0])
 
