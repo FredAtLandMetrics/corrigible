bf025dc724fd05c2723351cb535b94bc7cd3ddfd
Fred McDavid <fred@landmetrics.com>
2014-09-12 22:07:15 -0400

    no functional changes, just visibility enhancements

--- lib/selector.py
+++ lib/selector.py
@@ -1,13 +1,17 @@
 def run_selector_affirmative(selectors):
+    ret = False
     try:
-        assert(_run_selector_include_affirmative(selectors['include']))
-        return True
+        assert(bool(_run_selector_include_affirmative(selectors['include'])))
+        ret = True   # true if specifically included
     except (AssertionError, KeyError):
         try:
-            assert(_run_selector_exclude_affirmative(selectors['exclude']))
-            return False
+            assert(bool(_run_selector_exclude_affirmative(selectors['exclude'])))
+            ret = False  # false if specifically excluded
         except (AssertionError, KeyError):
-            return True
+            ret = True   # default true
+
+    print "run_selector_affirmative returning: {}".format(ret)
+    return ret
 
 _run_selectors = None
 def set_run_selectors_list(rslist):
@@ -21,38 +25,44 @@ def get_run_selectors_list():
 def _run_selector_include_affirmative(include_selectors):
 
     if (type(include_selectors) is not list):
-        return _run_selector_exclude_affirmative([include_selectors])
+        return _run_selector_include_affirmative([include_selectors])
 
+    ret = False
     try:
         run_selectors = get_run_selectors_list()
-        #print('_run_selector_include_affirmative - include_selectors: {}'.format(str(include_selectors)))
-        #print('_run_selector_include_affirmative - run_selectors: {}'.format(str(run_selectors)))
-        
+        print('_run_selector_include_affirmative - include_selectors: {}'.format(str(include_selectors)))
+        # print('_run_selector_include_affirmative - exclude_selectors: {}'.format(str(exclude_selectors)))
+        print('_run_selector_include_affirmative - run_selectors: {}'.format(str(run_selectors)))
+
         for sel in include_selectors:
             if sel == 'ALL':
-                return True
+                ret = True
             if sel in run_selectors:
-                return True
-        return False
+                ret = True
+
     except TypeError:
-        return False
+        pass
+    print "_run_selector_include_affirmative returning: {}".format(ret)
+    return ret
 
 def _run_selector_exclude_affirmative(exclude_selectors):
     
     if (type(exclude_selectors) is not list):
         return _run_selector_exclude_affirmative([exclude_selectors])
-    
+
+    ret = False
     try:
         run_selectors = get_run_selectors_list()
-        #print('_run_selector_exclude_affirmative - exclude_selectors: {}'.format(str(exclude_selectors)))
-        #print('_run_selector_exclude_affirmative - run_selectors: {}'.format(str(run_selectors)))
+        print('_run_selector_exclude_affirmative - exclude_selectors: {}'.format(str(exclude_selectors)))
+        print('_run_selector_exclude_affirmative - run_selectors: {}'.format(str(run_selectors)))
         for sel in exclude_selectors:
             if str(sel) == 'ALL':
-                return True
+                ret = True
             if str(sel) in run_selectors:
-                return True
-        return False
+                ret = True
     except TypeError:
-        return False
+        pass
+    print "_run_selector_exclude_affirmative returning: {}".format(ret)
+    return ret
 
 
