9a662df82e436be6e29698e17f7ef8d43e7a04b1
Fred McDavid <fred@landmetrics.com>
2014-12-02 00:37:32 -0500

    more verbosity

--- test/test_all.sh
+++ test/test_all.sh
@@ -3,19 +3,39 @@
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPTDIR
 
-./test_simple_system_config.py && \
-./test_complex_system_config.py && \
-./test_system_params.py && \
-./test_template_files.py && \
-./test_same_file_param_subst.py && \
-./test_plans_in_subdirs.py && \
-./test_files_param_subst.py && \
-./test_inline_ansible_plan.py && \
-./test_local_connect_for_testing.py && \
-./test_files_with_parameters.py && \
-./test_run_selectors.py
+echo "RUNNING: test_simple_system_config.py"
+$SCRIPTDIR/test_simple_system_config.py
 
-# NOT WORKING:
-#
-#   ./test_hashskip.py
-#
+echo "RUNNING: test_complex_system_config.py"
+$SCRIPTDIR/test_complex_system_config.py 
+
+echo "RUNNING: test_system_params.py"
+$SCRIPTDIR/test_system_params.py 
+
+echo "RUNNING: test_template_files.py"
+$SCRIPTDIR/test_template_files.py 
+
+echo "RUNNING: test_same_file_param_subst.py"
+$SCRIPTDIR/test_same_file_param_subst.py 
+
+echo "RUNNING: test_plans_in_subdirs.py"
+$SCRIPTDIR/test_plans_in_subdirs.py 
+
+echo "RUNNING: test_files_param_subst.py"
+$SCRIPTDIR/test_files_param_subst.py 
+
+echo "RUNNING: test_inline_ansible_plan.py"
+$SCRIPTDIR/test_inline_ansible_plan.py 
+
+echo "RUNNING: test_local_connect_for_testing.py"
+$SCRIPTDIR/test_local_connect_for_testing.py 
+
+echo "RUNNING: test_files_with_parameters.py"
+$SCRIPTDIR/test_files_with_parameters.py 
+
+echo "RUNNING: test_run_selectors.py"
+$SCRIPTDIR/test_run_selectors.py
+
+echo "RUNNING: test_hashskip.py"
+$SCRIPTDIR/test_hashskip.py
+
