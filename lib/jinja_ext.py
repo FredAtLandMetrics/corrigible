a2d98d2d091f41f513484b8b2684732243bf4d9f
Fred McDavid <fred@priceonomics.com>
2015-12-29 12:35:41 -0500

    added directory handling before and after calling the shell command (to be consistent with other corrigible features) and deleted the commented out code that didn't work

--- lib/jinja_ext.py
+++ lib/jinja_ext.py
@@ -1,6 +1,7 @@
 # from jinja2 import nodes
 # from jinja2.ext import Extension
 # from jinja2.exceptions import TemplateSyntaxError
+import os
 import subprocess
 import jinja2
 import jinjatag
@@ -9,37 +10,20 @@ env = jinja2.Environment(autoescape=False, extensions=[ShellExtension])
 ShellExtension.init()
 
 @jinjatag.simple_tag
-def shell(cmd, **kwargs):
+def shell(cmd, chdir=".", **kwargs):
+
+    original_dirpath = os.getcwd()
+    new_dirpath = os.environ["CORRIGIBLE_PATH"]
+    if chdir != ".":
+        if chdir[0] == "/":
+            new_dirpath = chdir
+        else:
+            new_dirpath = os.path.join(new_dirpath, chdir)
+    os.chdir(new_dirpath)
+
     output = subprocess.Popen(cmd.split(" "), stdout=subprocess.PIPE).communicate()[0].rstrip()
+
+    os.chdir(original_dirpath)
+
     return "'{}'".format(output)
 
-# class ShellExtension(Extension):
-#
-#     tags = set(['shell'])
-#
-#     def parse(self, parser):
-#
-#         stream = parser.stream
-#         tag = stream.next()  # this stores the 'shell' keyword
-#
-#         pieces = []
-#         while not stream.current.test_any('block_end', 'name:as'):
-#             pieces.append(stream.next())
-#
-#         cmd = ""
-#         if bool(pieces):
-#             cmd = "".join([b.value for b in pieces])
-#
-#         print "cmd: {}".format(cmd)
-#
-#         lineno = stream.current.lineno
-#         # return nodes.CallBlock(
-#         #     self.call_method(self, '_get_shell_output', cmd), [], [], []
-#         # ).set_lineno(lineno)
-#         #return nodes.Output([self.call_method('_get_shell_output', args=[nodes.Const(cmd)], kwargs={})]).set_lineno(lineno)
-#         return nodes.Const("test")
-#
-#     @classmethod
-#     def _get_shell_output(cls, cmd, *args, **kwargs):
-#         print "cmd1: {}".format(cmd)
-#         return 'doh'
