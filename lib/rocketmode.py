3664524e51826691853e7e95995c305af5ed7dc3
Fred McDavid <fred@frameworklabs.us>
2016-11-30 12:18:14 -0500

    lib/rocketmode.py: fixes a py2 print

--- lib/rocketmode.py
+++ lib/rocketmode.py
@@ -4,5 +4,5 @@ def rocket_mode(*args):
     global _rocket_mode
     if type(args) is tuple and len(args) > 0:
         _rocket_mode = args[0]
-    print "DBG: rocket_mode returning: {}".format(_rocket_mode)
+    print("DBG: rocket_mode returning: {}".format(_rocket_mode))
     return _rocket_mode
