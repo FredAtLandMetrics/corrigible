8a9fb35e05513ae0a3aa461c36ba44f95863b474
Fred McDavid <fred@frameworklabs.us>
2017-03-13 16:30:49 -0400

    lib/hostsfile.py: code cleanup and makes code a bit more idiomatic

--- lib/hostsfile.py
+++ lib/hostsfile.py
@@ -4,31 +4,20 @@ from .system import system_config
 from .dirpaths import temp_exec_dirpath
 
 def write_ansible_hosts(opts):
-    mconf = None
-    try:
-        mconf = system_config(opts)
-        hostrecs = mconf['hosts']
-        
-        hosts_filepath = ansible_hostsfile_filepath(opts)
-        with open(hosts_filepath, "w") as fh:
-            fh.write("[all]\n")
-            
-            for hostrec in hostrecs:
-                fh.write("{} ansible_ssh_host={}\n".format(hostrec['hostname'], hostrec['ip_address']))
-            fh.write("\n")
-    except TypeError:
-        if mconf is None:
-            print("ERR: No system config, not writing ansible hosts file")
-            return
-        else:
-            raise
+    mconf = system_config(opts)
+
+    hostrecs = mconf['hosts']
+
+    hosts_filepath = ansible_hostsfile_filepath(opts)
+    with open(hosts_filepath, "w") as fh:
+        fh.write("[all]\n")
+        for hostrec in hostrecs:
+            fh.write("{} ansible_ssh_host={}\n".format(hostrec['hostname'], hostrec['ip_address']))
+        fh.write("\n")
 
 def ansible_hostsfile_filepath(opts):
-    try:
-        output_filepath = opts["hosts_output_file"]
-        assert(output_filepath is not None)
-        return output_filepath
-    except (AssertionError, KeyError):
-        return os.path.join(temp_exec_dirpath(),
-                            "provision_{}.hosts".format(opts['system']))
+
+    if "hosts_output_file" in opts and bool(opts["hosts_output_file"]):
+        return opts["hosts_output_file"]
+    return os.path.join(temp_exec_dirpath(), "provision_{}.hosts".format(opts['system']))
 
