04c5f16d93bec42e861a245d0b1cdfd022c9c0fc
Fred McDavid <fred@frameworklabs.us>
2017-03-14 18:59:22 -0400

    test/test_system_params.py: minor code cleanup. adds function comment to the test method.

--- test/test_system_params.py
+++ test/test_system_params.py
@@ -8,18 +8,23 @@ from corrigible.lib.plan import plan_index, plan_filepath
 from corrigible.test.lib.corrigible_test import CorrigibleTest
 
 import corrigible.lib.plan
+
+
 script_dirpath = os.path.join(os.path.dirname(corrigible.lib.plan.__file__), '..', 'test')
 system_config_dirpath = os.path.join(script_dirpath,'resources','systems')
 plans_config_dirpath = os.path.join(script_dirpath,'resources','plans')
 files_config_dirpath = os.path.join(script_dirpath,'resources','files')
 corrigible_exec_filepath = os.path.join(script_dirpath, '..', 'corrigible')
 
+
 os.environ['CORRIGIBLE_SYSTEMS'] = system_config_dirpath
 os.environ['CORRIGIBLE_PLANS'] = plans_config_dirpath
 os.environ['CORRIGIBLE_FILES'] = files_config_dirpath
 
+
 PLAYBOOK_FILEPATH__SYSTEMCONF_TEST = "/tmp/corrigible-test-output.yml"
 HOSTS_FILEPATH__SYSTEMCONF_TEST = "/tmp/corrigible-test-hosts-output.hosts"
+
 
 class TestSystemParams(CorrigibleTest):
 
@@ -34,9 +39,10 @@ class TestSystemParams(CorrigibleTest):
                               generate_files_only=True)
         
     def test_system_parameters(self):
+        """test that the user in the second stanza is set to the same user that is running the tests, as
+            the sudouser in the parameter stanza of the system file is set to sys_local_user """
         self.regen()
         s = self.playbook_as_struct()
-        # print json.dumps(s, sort_keys=True, indent=4, separators=(',', ': '))
         self.assertTrue(type(s) is list)
         self.assertTrue(len(s) > 0)
         self.assertTrue(type(s[1]) is dict)
