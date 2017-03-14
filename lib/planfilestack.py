d1e4d4cbdff1019f33fd50a971e13d2d6a77c2c7
Fred McDavid <fred@frameworklabs.us>
2017-03-14 12:33:43 -0400

    lib/planfilestack.py: code cleanup. adds function comments.

--- lib/planfilestack.py
+++ lib/planfilestack.py
@@ -1,10 +1,18 @@
+# a simple plan stack to be used for troubleshooting unparseable plans
+
 _plan_name_stack = []
+
 def plan_file_stack_push(plan_name):
+    """pushes a plan name onto the stack"""
+    global _plan_name_stack
     _plan_name_stack.append(plan_name)
     
 def plan_file_stack_pop():
+    """pops a plan name off the stack"""
+    global _plan_name_stack
     _plan_name_stack.pop()
     
 def plan_file_stack_as_str():
+    """returns a string representation of the plan name stack"""
     return ">".join(_plan_name_stack)
 
