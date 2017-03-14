c899ec89d48acbfcc537f3be0b7e13988a137ba6
Fred McDavid <fred@frameworklabs.us>
2017-03-14 18:05:28 -0400

    lib/selector.py: simplifies run selector behavior and code

--- lib/selector.py
+++ lib/selector.py
@@ -8,17 +8,34 @@
 def run_selector_affirmative(selectors):
     """return whether to include a plan based on run selector information. default to True if no run selector applies."""
 
-    # includes
-    if 'include' in selectors and bool(selectors['include']):
-        if _run_selector_include_affirmative(selectors['include']):
-            return True
+    # gather include and exclude selectors
+    include_selectors = selectors['include'] if 'include' in selectors and bool(selectors['include']) else []
+    exclude_selectors = selectors['exclude'] if 'exclude' in selectors and bool(selectors['exclude']) else []
 
-    # excludes
-    if 'exclude' in selectors and bool(selectors['exclude']):
-        if _run_selector_exclude_affirmative(selectors['exclude']):
-            return False
+    # if no run selectors, only return false for exclude selector lists with ALL
+    run_selectors_list = get_run_selectors_list()
+    if run_selectors_list is None:
+        return False if 'ALL' in exclude_selectors else True
 
-    # default to True
+    # if ALL in exclude selectors return True only if there is an intersection between include selectors
+    #  and run selectors
+    if 'ALL' in exclude_selectors:
+        return bool(list(set(include_selectors) & set(run_selectors_list)))
+
+    # if ALL in include selectors return False only if there is an intersection between exclude selectors
+    #  and run selectors
+    if 'ALL' in include_selectors:
+        return not bool(list(set(exclude_selectors) & set(run_selectors_list)))
+
+    # return False if intersection between exclude selectors and run selectors
+    if bool(list(set(exclude_selectors) & set(run_selectors_list))):
+        return False
+
+    # return False if include selectors is not empty and there is no intersection between include and
+    # run_selectors
+    if bool(include_selectors):
+        return bool(list(set(include_selectors) & set(run_selectors_list)))
+
     return True
 
 
@@ -33,51 +50,4 @@ def get_run_selectors_list():
     """returns the run selectors list"""
     global _run_selectors
     return _run_selectors
-
-
-def _run_selector_include_affirmative(include_selectors):
-    """given an include list of selectors from a run selector record,
-          if one of the selectors is ALL, return True
-          if instead, any one of the selectors is in the run selectors list for this process, return True
-          otherwise return False
-    """
-    # handle non-list include_selectors param
-    if (type(include_selectors) is not list):
-        return _run_selector_exclude_affirmative([include_selectors])
-
-    # if one of the selectors is ALL, return True
-    if "ALL" in include_selectors:
-        return True
-
-    # default to false if run_selectors_list is None
-    run_selectors_list = get_run_selectors_list()
-    if run_selectors_list is None:
-        return False
-
-    # return true if the intersection of the two lists contains any members
-    return bool(list(set(include_selectors) & set(run_selectors_list)))
-
-def _run_selector_exclude_affirmative(exclude_selectors):
-    """given an exclude list of selectors from a run selector record,
-        if one of the selectors is ALL, return True
-        if instead, any one of the selectors is in the run selectors list for this process, return True
-        otherwise return False
-    """
-
-    # handle non-list exclude_selectors list
-    if (type(exclude_selectors) is not list):
-        return _run_selector_exclude_affirmative([exclude_selectors])
-
-    # if one of the selectors is ALL, return True
-    if "ALL" in exclude_selectors:
-        return True
-
-    # default to false if run_selectors_list is None
-    run_selectors_list = get_run_selectors_list()
-    if run_selectors_list is None:
-        return False
-
-    # return true if the intersection of the two lists contains any members
-    return bool(list(set(exclude_selectors) & set(run_selectors_list)))
-
 
