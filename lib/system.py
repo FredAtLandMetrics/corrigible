cc3448d437400539d396412d56a65a963b18c713
Fred McDavid <fred@frameworklabs.us>
2017-03-14 15:57:06 -0400

    lib/system.py: code cleanup.

--- lib/system.py
+++ lib/system.py
@@ -9,69 +9,67 @@ from .dirpaths import systems_dirpath
 from .selector import run_selector_affirmative
 from .sys_default_params import sys_default_parameters
 
+
+# init jinja environment
 env = jinja2.Environment(autoescape=False)
+
 
 SYSTEM_FILE_SUFFIX = "system"
 
+
 _system_conf = None
+
+
 def system_config(opts):
+    """return the processed systemconfig yaml as a struct"""
     global _system_conf
+
+    # just return the config struct if this has been done before
+    if _system_conf is not None:
+        return _system_conf
+
+    # determine full system config filepath
+    system_name = opts["system"]
+    plan_file_stack_push(system_name)
+    system_config_filepath = os.path.join(systems_dirpath(), "{}.{}".format(system_name, SYSTEM_FILE_SUFFIX))
+
+    # read the system config to string
     try:
-        if _system_conf is None:
-            system_name = opts["system"]
-            plan_file_stack_push(system_name)
-            system_config_filepath = os.path.join(systems_dirpath(), "{}.{}".format(system_name, SYSTEM_FILE_SUFFIX))
-            print("INFO: loading system config for: {}, at {}".format(system_name, system_config_filepath))
-            with open (system_config_filepath, "r") as system_def_fh: 
-                
-                params = dict(list(sys_default_parameters().items()) + list(os.environ.items()))
-
-                params["CMDLINE"] = os.environ["_"]
-                params["CMDLINE_WITHOUT_SELECTORS"] = re.sub(r'--selectors=[^\s]+','', params["CMDLINE"])
-                
-                unrendered_system_def_str = system_def_fh.read()
-                pass1_rendered_system_def_str = \
-                    env.from_string(unrendered_system_def_str).render(**params)
-                
-                # get params in pass1
-                temp_conf = yaml.load(pass1_rendered_system_def_str)
-                try:
-                    parameter_dict = copy.copy(temp_conf['parameters'])
-                except KeyError:
-                    parameter_dict = None
-
-                # merge in parameters (with os.environ trumping parameters)
-                if parameter_dict is None:
-                    render_params = params
-                else:
-                    render_params = dict(
-                        list(parameter_dict.items()) + \
-                        list(os.environ.items()) + \
-                        list(sys_default_parameters().items())
-                    )
-
-                # final system config load
-                pass2_rendered_system_def_str = env.from_string(unrendered_system_def_str).render(**render_params)
-
-                # filter the output str (remove hosts that are excluded via selectors)
-                rendered_system_def_str = _filter_system_def(pass2_rendered_system_def_str, opts)
-                
-                _system_conf = yaml.load(rendered_system_def_str)
-                
+        with open (system_config_filepath, "r") as system_def_fh:
+            unrendered_system_def_str = system_def_fh.read()
     except IOError:
         print("\nERR: system config not found at: {}, system_config will be None\n".format(system_config_filepath))
-    return _system_conf
+        return None
 
-    
-def _filter_system_def(raw, opts):
-    system_conf = yaml.load(raw)
-    
-    hosts_list = system_conf['hosts']
+    # build the pass1_render_params dict
+    pass1_render_params = dict(list(sys_default_parameters().items()) + list(os.environ.items()))
+    pass1_render_params["CMDLINE"] = os.environ["_"]
+    pass1_render_params["CMDLINE_WITHOUT_SELECTORS"] = re.sub(r'--selectors=[^\s]+','', pass1_render_params["CMDLINE"])
+
+    # pass 1: read to struct as is, extract parameters section for pass 2
+    pass1_rendered_system_def_str = env.from_string(unrendered_system_def_str).render(**pass1_render_params)
+    pass_1_system_dict = yaml.load(pass1_rendered_system_def_str)
+    parameter_dict = None if 'parameters' not in pass_1_system_dict else pass_1_system_dict['parameters']
+
+    # build the pass2_render_params_dict
+    pass2_render_params = pass1_render_params \
+        if parameter_dict is None \
+        else dict(list(parameter_dict.items()) + list(pass1_render_params.items()))
+
+    # pass 2: render the yaml using parameter from environment and from system file itself
+    pass2_rendered_system_def_str = env.from_string(unrendered_system_def_str).render(**pass2_render_params)
+
+    # filter the rendered yaml str (remove hosts that are excluded via selectors)
+    unfiltered_config_dict = yaml.load(pass2_rendered_system_def_str)
+    hosts_list = unfiltered_config_dict['hosts']
     new_hosts_list = []
     for host in hosts_list:
         if 'run_selectors' in host and not run_selector_affirmative(host['run_selectors']):
             continue
         new_hosts_list.append(host)
-    system_conf['hosts'] = new_hosts_list    
-    return yaml.dump(system_conf)
+    unfiltered_config_dict['hosts'] = new_hosts_list
 
+    # load the filtered yaml string to a dict
+    _system_conf = unfiltered_config_dict
+
+    return _system_conf
