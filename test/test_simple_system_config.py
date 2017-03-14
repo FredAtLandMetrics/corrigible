c81013d18fad96304189190753eee39e07d09302
Fred McDavid <fred@frameworklabs.us>
2017-03-14 18:58:49 -0400

    test/test_simple_system_config.py: minor code cleanup

--- test/test_simple_system_config.py
+++ test/test_simple_system_config.py
@@ -8,18 +8,23 @@ from corrigible.lib.plan import plan_index, plan_filepath
 from corrigible.test.lib.corrigible_test import CorrigibleTest
 
 import corrigible.lib.plan
+
+
 script_dirpath = os.path.join(os.path.dirname(corrigible.lib.plan.__file__), '..', 'test')
 system_config_dirpath = os.path.join(script_dirpath,'resources','systems')
 plans_config_dirpath = os.path.join(script_dirpath,'resources','plans')
 files_config_dirpath = os.path.join(script_dirpath,'resources','files')
 corrigible_exec_filepath = os.path.join(script_dirpath, '..', 'corrigible')
 
+
 os.environ['CORRIGIBLE_SYSTEMS'] = system_config_dirpath
 os.environ['CORRIGIBLE_PLANS'] = plans_config_dirpath
 os.environ['CORRIGIBLE_FILES'] = files_config_dirpath
 
+
 PLAYBOOK_FILEPATH__SYSTEMCONF_TEST = "/tmp/corrigible-test-output.yml"
 HOSTS_FILEPATH__SYSTEMCONF_TEST = "/tmp/corrigible-test-hosts-output.hosts"
+
 
 class TestSimpleSystemConfig(CorrigibleTest):
 
@@ -39,13 +44,15 @@ class TestSimpleSystemConfig(CorrigibleTest):
                               generate_files_only=True)
         
     def test_system_config_output_files_exist(self):
-        """test that the output files exist after rerunning corrigible using the hostsfile generation test system config"""
+        """test that the output files exist after rerunning corrigible using the hostsfile generation test system
+            config"""
         self.regen_test_simple_plans()
         self.assertTrue(os.path.isfile(self.output_playbook_filepath))
         self.assertTrue(os.path.isfile(self.output_hostsfile_filepath))
         
     def test_system_config_hosts_file_accurate(self):
-        """test that the generated hosts file is accurate after rerunning corrigible using the hostsfile generation test system config"""
+        """test that the generated hosts file is accurate after rerunning corrigible using the hostsfile generation
+            test system config"""
         self.regen_test_hostsfile_gen_files()
         hostgroups = self.hosts_groups_from_file(self.output_hostsfile_filepath)
         self.assertTrue('all' in hostgroups)
@@ -55,30 +62,33 @@ class TestSimpleSystemConfig(CorrigibleTest):
         self.assertTrue(lines[0] == "testhost ansible_ssh_host=1.2.3.4")
         
     def test_plan_index(self):
-        """test that the plan index method in the provision files lib is properly returning the index specified in the plan filename"""
+        """test that the plan index method in the provision files lib is properly returning the index specified in
+            the plan filename"""
         dt_index = plan_index('plans_test')
         self.assertTrue(dt_index == 57)
         d_index = plan_index('apt_upgrade')
         self.assertTrue(d_index == 19)
         
     def test_plan_filepath(self):
-        """test that the plan filepath method in the provision files lib is properly returning the filepath to the plan file"""
+        """test that the plan filepath method in the provision files lib is properly returning the filepath to the
+            plan file"""
         dt_filepath = plan_filepath('plans_test');
-        #print "dt_filepath: {}".format(dt_filepath)
-        self.assertTrue(dt_filepath == os.path.abspath(os.path.join(script_dirpath,'resources','plans','57_plans_test.plan.yml')))
+        computed_filepath = os.path.abspath(os.path.join(script_dirpath,'resources','plans','57_plans_test.plan.yml'))
+        self.assertTrue(dt_filepath == computed_filepath)
         
     def test_parameter_substitution(self):
-        """after re-running corrigible on the simple plans test system config, test that basic parameter substitution is working"""
+        """after re-running corrigible on the simple plans test system config, test that basic parameter
+            substitution is working"""
         self.regen_test_simple_plans()
         self.assertTrue(os.path.isfile(self.output_playbook_filepath))
         self.assertTrue(os.path.isfile(self.output_hostsfile_filepath))
         s = self.playbook_as_struct()
-        # print json.dumps(s, sort_keys=True, indent=4, separators=(',', ': '))
         self.assertTrue(s[1]['user'] == 'ubuntu')
         self.assertTrue(s[1]['sudo'] == True)
 
     def test_plan_ordering_by_index(self):
-        """after re-running corrigible on the simple plans test system config, test that the plans are ordered as per the index indicated is each's filename"""
+        """after re-running corrigible on the simple plans test system config, test that the plans are ordered
+            as per the index indicated is each's filename"""
         self.regen_test_simple_plans()
         self.assertTrue(os.path.isfile(self.output_playbook_filepath))
         self.assertTrue(os.path.isfile(self.output_hostsfile_filepath))
