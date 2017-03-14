156dbe7f990eb761a46eca20f8cdd0649298c8b4
Fred McDavid <fred@frameworklabs.us>
2017-03-14 15:24:13 -0400

    lib/sys_default_params.py: adds comments

--- lib/sys_default_params.py
+++ lib/sys_default_params.py
@@ -1,4 +1,8 @@
+# this module allows for the definition of any variables that need to be provided to the system files by corrigible
+
 import os
 
 def sys_default_parameters():
+    """returns a dict of corrigible system variables to be made available to the templating engine for the system and
+        plan files"""
     return { 'sys_local_user': os.environ['USER'] }
