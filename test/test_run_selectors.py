9994337e9cd66a709b04cd81763945894eab810c
Fred McDavid <fred@frameworklabs.us>
2017-03-14 15:25:30 -0400

    test/test_run_selectors.py: updates the commented-out debugging stmts to use py3-style print

--- test/test_run_selectors.py
+++ test/test_run_selectors.py
@@ -7,6 +7,7 @@ from corrigible.lib.plan import plan_index, plan_filepath
 from corrigible.test.lib.corrigible_test import CorrigibleTest
 
 import corrigible.lib.plan
+
 script_dirpath = os.path.join(os.path.dirname(corrigible.lib.plan.__file__), '..', 'test')
 system_config_dirpath = os.path.join(script_dirpath,'resources','systems')
 plans_config_dirpath = os.path.join(script_dirpath,'resources','plans')
@@ -20,7 +21,9 @@ os.environ['CORRIGIBLE_FILES'] = files_config_dirpath
 PLAYBOOK_FILEPATH__SYSTEMCONF_TEST = "/tmp/corrigible-test-output.yml"
 HOSTS_FILEPATH__SYSTEMCONF_TEST = "/tmp/corrigible-test-hosts-output.hosts"
 
+
 class TestRunSelectors(CorrigibleTest):
+
     def setUp(self):
         self.output_playbook_filepath = PLAYBOOK_FILEPATH__SYSTEMCONF_TEST
         self.output_hostsfile_filepath = HOSTS_FILEPATH__SYSTEMCONF_TEST
@@ -28,10 +31,10 @@ class TestRunSelectors(CorrigibleTest):
         
     def _assert_hostsmap(self, incmap):
         hosts = self.hostgroup_hostnames(self.output_hostsfile_filepath, 'all')
-        #print "hosts: {}".format(str(hosts))
+        # print("hosts: {}".format(str(hosts)))
         for res_pair in incmap:
             hostname, present = res_pair
-            #print "testing host: {}, present: {}".format(hostname, present)
+            # print("testing host: {}, present: {}".format(hostname, present))
             if present is None or present is False:
                 self.assertFalse(hostname in hosts)
             elif present is True:
