b8a022ec3a160cb90c91f320d18f9dec2baa2d43
Fred McDavid <fred@frameworklabs.us>
2017-03-14 11:14:57 -0400

    lib/dirpaths.py: code cleanup

--- lib/dirpaths.py
+++ lib/dirpaths.py
@@ -1,39 +1,43 @@
 import os
 import tempfile
 
-def envvar_cap_var(environment_variable_name, **kwargs):
+# the temp execution dirpath
+tmp_exec_dirpath = None
+
+def deduce_dirpath(environment_variable_name, **kwargs):
+    """if the envvar is defined, return it otherwise return the default"""
     try:
         return os.environ[environment_variable_name]
     except KeyError:
-        try:
-            return kwargs['default']
-        except KeyError:
-            return None
-    
+        return kwargs['default'] if 'default' in kwargs else None
+
 def systems_dirpath():
-    return envvar_cap_var('CORRIGIBLE_SYSTEMS',default='{}/systems'.format(corrigible_path()))
+    """returns the dirpath where the systems files will be found"""
+    return deduce_dirpath('CORRIGIBLE_SYSTEMS', default='{}/systems'.format(corrigible_path()))
 
 def plans_dirpath():
-    return envvar_cap_var('CORRIGIBLE_PLANS',default='{}/plans'.format(corrigible_path()))
+    """returns the dirpath where the systems files will be found"""
+    return deduce_dirpath('CORRIGIBLE_PLANS', default='{}/plans'.format(corrigible_path()))
 
 def files_dirpath():
-    return envvar_cap_var('CORRIGIBLE_FILES',default='{}/files'.format(corrigible_path()))
+    """returns the dirpath where the systems files will be found"""
+    return deduce_dirpath('CORRIGIBLE_FILES', default='{}/files'.format(corrigible_path()))
 
 def corrigible_path():
-    return envvar_cap_var('CORRIGIBLE_PATH',default='/usr/local/etc/corrigible')
+    """returns the dirpath where the systems, plans and files directories will be found"""
+    return deduce_dirpath('CORRIGIBLE_PATH', default='/usr/local/etc/corrigible')
 
 def hashes_dirpath():
-    try:
-        return os.environ['CORRIGIBLE_HASHES']
-    except KeyError:
-        return "/corrigible/hashes"
+    """returns the dirpath where the plan hashes will be found"""
+    return os.environ['CORRIGIBLE_HASHES'] if 'CORRIGIBLE_HASHES' in os.environ else "/corrigible/hashes"
 
-
-_temp_exec_dir = None
 def temp_exec_dirpath():
-    global _temp_exec_dir
-    if _temp_exec_dir is None:
-        _temp_exec_dir = tempfile.mkdtemp()
-        os.symlink(files_dirpath(),
-                   os.path.join(_temp_exec_dir,os.path.basename(files_dirpath())))
-    return _temp_exec_dir
+    """returns the dirpath to the temp execution directory for this process"""
+    global tmp_exec_dirpath
+
+    # if the temp exec dirpath is still none, create the directory and symlink the files directory
+    if tmp_exec_dirpath is None:
+        tmp_exec_dirpath = tempfile.mkdtemp()
+        os.symlink(files_dirpath(), os.path.join(tmp_exec_dirpath, os.path.basename(files_dirpath())))
+        
+    return tmp_exec_dirpath
